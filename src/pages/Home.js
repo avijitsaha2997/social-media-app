05404c69d86af255f255157f93ab0a08c59f559c
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-04-27 02:05:01 +0600

    Start profileSide comp

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div className="home">
+      <div className="profileSide">Profile</div>
+      <div className="postSide">Posts</div>
+      <div className="rightSide">RightSide</div>
+    </div>
+  );
+}
+
+export default Home;
