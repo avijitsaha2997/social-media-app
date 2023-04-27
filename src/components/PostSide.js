9611824b8999ef9acefaaaf02fd7e312395ee65e
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-04-27 23:24:32 +0600

    postshare done

--- src/components/PostSide.js
+++ src/components/PostSide.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./PostSide.css";
+import PostShare from "./PostShare";
 function PostSide() {
-  return <div>PostSide</div>;
+  return (
+    <div className="postSide">
+      <PostShare />
+      {/* <Posts /> */}
+    </div>
+  );
 }
 
 export default PostSide;
