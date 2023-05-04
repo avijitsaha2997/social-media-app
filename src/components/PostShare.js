e9e2def85fcb3b90e7377257d67443a6f88b9dec
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-05-04 21:11:59 +0600

    share post done

--- src/components/PostShare.js
+++ src/components/PostShare.js
@@ -6,17 +6,77 @@ import { UilPlayCircle } from "@iconscout/react-unicons";
 import { UilLocationPoint } from "@iconscout/react-unicons";
 import { UilSchedule } from "@iconscout/react-unicons";
 import { UilTimes } from "@iconscout/react-unicons";
+import { useDispatch, useSelector } from "react-redux";
+import { uploadImage, uploadPost } from "../actions/uploadAction";
 function PostShare() {
   const [image, setImage] = useState(null);
   const imageRef = useRef();
+  const desc = useRef();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const loading = useSelector((state) => state.postReducer.uploading);
 
   const onImageChange = (event) => {
     if (event.target.files && event.target.files[0]) {
       let img = event.target.files[0];
-      setImage({
-        image: URL.createObjectURL(img),
-      });
+      setImage(img);
     }
+  };
+
+  // handle post upload
+  const handleUpload = async (e) => {
+    e.preventDefault();
+
+    const newPost = {
+      userId: user._id,
+      desc: desc.current.value,
+    };
+
+    if (image) {
+      const data = new FormData();
+      const fileName = Date.now() + image.name;
+      data.append("name", fileName);
+      data.append("file", image);
+      newPost.image = fileName;
+      console.log(newPost);
+      try {
+        dispatch(uploadImage(data));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    dispatch(uploadPost(newPost));
+    resetShare();
+  };
+
+  //post data
+  // const newPost = {
+  //   userId: user._id,
+  //   desc: desc.current.value,
+  // };
+
+  // if there is an image with post
+  //   if (image) {
+  //     const data = new FormData();
+  //     const fileName = Date.now() + image.name;
+  //     data.append("name", fileName);
+  //     data.append("file", image);
+  //     newPost.image = fileName;
+  //     console.log(newPost);
+  //     try {
+  //       dispatch(uploadImage(data));
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   }
+  //   dispatch(uploadPost(newPost));
+  //   resetShare();
+  // };
+
+  // Reset Post Share
+  const resetShare = () => {
+    setImage(null);
+    desc.current.value = "";
   };
   return (
     <div className="postShare">
@@ -25,7 +85,7 @@ function PostShare() {
         alt=""
       />
       <div>
-        <input type="text" placeholder="What's happening" />
+        <input type="text" placeholder="What's happening" required ref={desc} />
         <div className="postOptions">
           <div
             className="option"
@@ -47,7 +107,13 @@ function PostShare() {
             <UilSchedule />
             Shedule
           </div>
-          <button className="button ps-button">Share</button>
+          <button
+            className="button ps-button"
+            onClick={handleUpload}
+            disabled={loading}
+          >
+            {loading ? "uploading" : "Share"}
+          </button>
           <div style={{ display: "none" }}>
             <input
               type="file"
@@ -61,7 +127,7 @@ function PostShare() {
         {image && (
           <div className="previewImage">
             <UilTimes onClick={() => setImage(null)} />
-            <img src={image.image} alt="" />
+            <img src={URL.createObjectURL(image)} alt="" />
           </div>
         )}
       </div>
