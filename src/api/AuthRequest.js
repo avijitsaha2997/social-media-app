c99a8e35d2ec40853d6ffe29799a052475a89d37
avijitsaha2997 <jit.avi.saha2997@gmail.com>
2023-05-16 19:42:33 +0600

    update all baseurl

--- src/api/AuthRequest.js
+++ src/api/AuthRequest.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:8000" });
+const API = axios.create({
+  baseURL: "https://social-media-app-server-5y0z.onrender.com/",
+});
 
 export const logIn = (formData) => API.post("/auth/login", formData);
 
